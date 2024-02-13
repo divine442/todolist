f8ab180de77a380c9c16d3f451ae1e6fedc35a77
Your Name <you@example.com>
2024-02-13 14:38:21 +0100

    first commit

---
+++ src/Components/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+  return (
+    <div className='Todo'>
+        <p onClick={() => toggleComplete(task.id)}  className={`${task.completed ? 'completed': 'incompleted'}`}>{task.task}</p>
+        <div>
+            <FontAwesomeIcon icon={faPenToSquare} onClick={()=> editTodo(task.id)}/>
+            <FontAwesomeIcon onClick={()=> deleteTodo(task.id)} icon={faTrash}/>
+        </div>
+    </div>
+  )
+}
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+
+    const [value, setValue] = useState("")
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        addTodo(value)
+        setValue("")
+
+    }
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input className='todo-input' type='text' value={value} placeholder='What is the task today' onChange={(e)=> setValue(e.target.value)}></input>
+        <button type='submit' className='todo-btn'>ADD TODO</button>
+    </form>
+  )
+}
